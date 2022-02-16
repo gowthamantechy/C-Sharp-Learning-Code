bd69d10c320f63fbad8ff671c8c5807d8e7b5399
Gowthaman <gowthamanvai@gmail.com>
2022-02-16 23:52:00 +0530

    Dynamic binding features added

--- Features/CSharpFeatures/CSharp4.0/DynamicBindingEx.cs
+++ Features/CSharpFeatures/CSharp4.0/DynamicBindingEx.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace CSharpFeatures.CSharp4._0
 {
-    internal class DynamicBindingEx
+
+    public class Student
     {
+        // Creating dynamic property  
+        public dynamic Name { get; set; }
+        // Creating a dynamic method  
+        public dynamic ShowMSG(string msg)
+        {
+            return msg;
+        }
+    }
+
+    public class DynamicBindingEx
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Micheal";
+            Console.WriteLine(student.Name);
+            // Storing result in dynamic object  
+            dynamic msg = student.ShowMSG("Welcome to Dynamic Binding");
+            Console.WriteLine(msg);
+
+            Console.ReadKey();
+        }
     }
 }
