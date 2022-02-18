ea88674392333cf52910669e847a47fad23dfb96
Gowthaman <gowthamanvai@gmail.com>
2022-02-18 19:58:16 +0530

    non coalecing operator test

--- Features/CSharpFeatures/CSharp8.0/NonCoalecingEx.cs
+++ Features/CSharpFeatures/CSharp8.0/NonCoalecingEx.cs
@@ -12,15 +12,33 @@ namespace CSharpFeatures.CSharp8._0
               
         public static void Main()
         {
-            List<int> numbers = null;
-            int? i = null;
-             
-            //numbers ??= new List<int>();
-            //numbers.Add(i ??= 17);
-            //numbers.Add(i ??= 20);
 
-            Console.WriteLine(string.Join(" ", numbers));  // output: 17 17
-            Console.WriteLine(i);  // output: 17
+            string str1 = null;
+
+            string str2 = null;
+
+            string str3 = null;
+
+            string str4 = "Hello, World";
+
+            string final = str1 ?? str2 ?? str3 ?? str4;
+
+            Console.WriteLine($"The final is : {final}");
+
+
+
+            /* List<int> numbers = null;
+             int? i = null;
+
+             //numbers ??= new List<int>();
+             //numbers.Add(i ??= 17);
+             //numbers.Add(i ??= 20);
+
+             Console.WriteLine(string.Join(" ", numbers));  // output: 17 17
+             Console.WriteLine(i);  // output: 17*/
+
+            Console.ReadKey();
+
         }
         
     }
